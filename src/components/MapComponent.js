42f3bb2160e0d564aa5b4a51b606ede5597072d0
Bilal-billu <bilalmalikoff@gmail.com>
2024-01-25 13:40:32 +0500

    Fixed pointer issues.

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -10,9 +10,9 @@ function MapComponent(props) {
     useEffect(() => {
         const map = L.map('map').setView([lat, long], 13);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-        // const marker = L.marker([lat, long]).addTo(map);
+        //const marker = L.marker([lat, long]).addTo(map);
         const customIcon = L.icon({
-            iconUrl: 'https://media.tenor.com/WpdEISWvPDwAAAAi/shigure-ui-ui-shigure.gif', // Replace with the path to your custom marker image
+            iconUrl: '/shigure-ui-pointer.gif', // Replace with the path to your custom marker image
             iconSize: [280, 200], // Set the size of the icon
             iconSize: [280, 200], // Set the size of the icon
             iconAnchor: [125, 200], // Set the anchor point for the icon
@@ -21,7 +21,7 @@ function MapComponent(props) {
 
 
         // const customIcon = L.icon({
-        //     iconUrl: 'https://cdn-icons-png.flaticon.com/512/535/535239.png',
+        //     iconUrl: '/pointer.png',
         //     iconSize: [30, 30], // Set the size of the icon
         //     iconAnchor: [16, 38], // Set the anchor point for the icon
 
